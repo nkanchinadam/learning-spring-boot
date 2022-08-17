3fdb76254f01710b5dff417e6ff7e745148b9da5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-08-17 19:09:25 -0400

    create FakePersonDataAccessService.java

---
+++ springboot/src/main/java/com/learning/springboot/dao/FakePersonDataAccessService.java
@@ -0,0 +1,17 @@
+package com.learning.springboot.dao;
+
+import com.learning.springboot.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FakePersonDataAccessService implements PersonDao {
+  private static List<Person> DB = new ArrayList<Person>();
+
+  @Override
+  public int insertPerson(UUID id, Person person) {
+    DB.add(new Person(id, person.getName()));
+    return 1;
+  }
+}
