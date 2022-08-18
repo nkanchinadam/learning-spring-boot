82e98fa026f8141de0d1a1afec7a7487e3ab919c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-08-18 11:49:49 -0400

    added annnotations to convert JSON data into Person objects

--- springboot/src/main/java/com/learning/springboot/api/PersonController.java
+++ springboot/src/main/java/com/learning/springboot/api/PersonController.java
@@ -2,11 +2,14 @@ package com.learning.springboot.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.springboot.model.Person;
 import com.learning.springboot.service.PersonService;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
   private final PersonService personService;
@@ -17,7 +20,7 @@ public class PersonController {
   }
 
   @PostMapping
-  public void addPerson(Person person) {
+  public void addPerson(@RequestBody Person person) {
     personService.addPerson(person);
   }
 }
