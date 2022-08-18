82ea1d5d9b9dc8dd59e10b9b0e772fc6ca5312d3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-08-18 12:22:40 -0400

    added GET request functionality to select all people

--- springboot/src/main/java/com/learning/springboot/api/PersonController.java
+++ springboot/src/main/java/com/learning/springboot/api/PersonController.java
@@ -1,6 +1,9 @@
 package com.learning.springboot.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,10 @@ public class PersonController {
   @PostMapping
   public void addPerson(@RequestBody Person person) {
     personService.addPerson(person);
+  }
+
+  @GetMapping
+  public List<Person> getAllPeople() {
+    return personService.getAllPeople();
   }
 }
