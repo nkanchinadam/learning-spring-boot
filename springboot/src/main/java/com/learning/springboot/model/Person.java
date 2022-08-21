094ef58415d38cb892eb002c2ca2d71ec6ed3409
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-08-20 22:59:14 -0400

    added validity check annotations to Person.java and PersonController.java

--- springboot/src/main/java/com/learning/springboot/model/Person.java
+++ springboot/src/main/java/com/learning/springboot/model/Person.java
@@ -2,10 +2,15 @@ package com.learning.springboot.model;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Person {
   private final UUID id;
+
+  @NotBlank
   private final String name;
 
   public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
